00603243e1b4c56fc9a5b451cfbd9ee6c8a34245
Pablo Calderon <pablo_tiu@outlook.com>
2022-07-30 08:22:14 -0300

    se agrego prueba al menu

--- src/utils/Menu.tsx
+++ src/utils/Menu.tsx
@@ -23,6 +23,9 @@ export default function Menu(){
                         <li className="nav-item">
                             <NavLink to="/peliculas/Crear" className="nav-link">Crear Peliculas</NavLink>
                         </li>
+                        <li className="nav-item">
+                            <NavLink to="/" className="nav-link">Prueba</NavLink>
+                        </li>
                     </ul>
                 </div>
             </div>
